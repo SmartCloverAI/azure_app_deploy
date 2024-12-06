4aa69b42afb7a0389c5ee748190211524595f7d9
ispirtraian <traian.ispir@yahoo.com>
2024-12-06 18:15:57 +0200

    add: test_data

--- az_scripts/aimethica_ml_endpoints_scrap.sh
+++ az_scripts/aimethica_ml_endpoints_scrap.sh
@@ -4,3 +4,7 @@ az ml online-deployment show --name tenderrootwdxkv267-1 --endpoint-name weekly-
 az ml environment show --name aimethica-anomaly-env --version 7 --resource-group AiMethica-ML --workspace-name AiMethica-AZML -o yaml > aimethica-anomaly-env.yaml
 
 az ml online-deployment create -f tenderrootwdxkv267-1.yaml --resource-group AiMethica-ML --workspace-name AiMethica-AZML
+az ml online-deployment get-logs --endpoint-name anomaly-document-nir --name tff-blue --lines 100 --resource-group AiMethica-ML --workspace-name AiMethica-AZML
+
+az ml workspace show  --resource-group AiMethica-ML --name AiMethica-AZML
+
